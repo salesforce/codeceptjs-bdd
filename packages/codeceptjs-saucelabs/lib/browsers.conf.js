d768c3621ea670435d805ee1bf70702c8ef78ccc
Kushang Gajjar <g.kushang@gmail.com>
2019-12-31 11:41:57 -0500

    allow insecure certs

--- packages/codeceptjs-saucelabs/lib/browsers.conf.js
+++ packages/codeceptjs-saucelabs/lib/browsers.conf.js
@@ -7,12 +7,14 @@ let browsers = {
     chrome: {
         browser: 'chrome',
         capabilities: {
+            acceptInsecureCerts: true,
             'sauce:options': sauceOptions
         }
     },
     firefox: {
         browser: 'firefox',
         capabilities: {
+            acceptInsecureCerts: true,
             'sauce:options': sauceOptions
         }
     },
@@ -20,24 +22,28 @@ let browsers = {
         browser: 'safari',
         capabilities: {
             browserVersion: '12.0',
+            acceptInsecureCerts: true,
             'sauce:options': sauceOptions
         }
     },
     edge: {
         browser: 'MicrosoftEdge',
         capabilities: {
+            acceptInsecureCerts: true,
             'sauce:options': sauceOptions
         }
     },
     ie: {
         browser: 'internet explorer',
         capabilities: {
+            acceptInsecureCerts: true,
             'sauce:options': sauceOptions
         }
     },
     mobileSimulator: {
         browser: 'chrome',
         desiredCapabilities: {
+            acceptInsecureCerts: true,
             chromeOptions: {
                 mobileEmulation: {
                     deviceName: "iPhone X"
@@ -48,6 +54,7 @@ let browsers = {
     tabletSimulator: {
         browser: 'chrome',
         desiredCapabilities: {
+            acceptInsecureCerts: true,
             chromeOptions: {
                 mobileEmulation: {
                     deviceName: "Kindle Fire HDX"
