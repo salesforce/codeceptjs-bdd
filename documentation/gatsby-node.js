40077aaa0ba6d65d8fdf2ba00e0cba1847949179
Kushang Gajjar <kgajjar@salesforce.com>
2020-02-03 11:46:56 -0500

    next prev links to docs template

--- documentation/gatsby-node.js
+++ documentation/gatsby-node.js
@@ -1,40 +1,56 @@
-const path = require('path');
-const { createFilePath } = require('gatsby-source-filesystem');
+const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions;
+  const { createNodeField } = actions
 
-    if (node.internal.type === 'MarkdownRemark') {
-        const slug = createFilePath({ node, getNode, basePath: `pages` });
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        });
-    }
-};
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions;
-    const result = await graphql(`
+  const { createPage } = actions
+  const result = await graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark (sort: { order: ASC, fields: [fileAbsolutePath] }) {
         nodes {
           fields {
             slug
           }
+          frontmatter {
+            title
+            parents
+          }
         }
       }
     }
-  `);
+  `)
 
-    result.data && result.data.allMarkdownRemark.nodes.forEach(node => {
-        createPage({
-                       path: node.fields.slug,
-                       component: path.resolve('./src/templates/DocPage.js'),
+  if (result.data) {
+    const posts = result.data.allMarkdownRemark.nodes;
+    console.log('posts: ', posts);
+    posts.forEach(({fields,frontmatter}, index) => {
+      createPage({
+        path: fields.slug,
+        component: path.resolve("./src/templates/DocPage.js"),
         context: {
-        slug: node.fields.slug
-    }
-});
-});
-};
+          slug: fields.slug,
+          prev: {
+            fields: index === 0 ? null : posts[index - 1].fields,
+            frontmatter: index === 0 ? null : posts[index - 1].frontmatter
+          },
+          next: {
+            fields: index === posts.length - 1 ? null : posts[index + 1].fields,
+            frontmatter: index === posts.length -1 ? null : posts[index + 1].frontmatter
+          }
+        },
+      })
+    })
+  }
+}
--- documentation/src/templates/DocPage.js
+++ documentation/src/templates/DocPage.js
@@ -1,48 +1,68 @@
-import React from 'react';
-import {graphql} from 'gatsby';
-import rehypeReact from 'rehype-react';
-import AppLayout from '../components/AppLayout';
-import Divider from '@material-ui/core/Divider';
-import {makeStyles} from "@material-ui/core";
-
-import Typography from "@material-ui/core/Typography";
+import React from "react"
+import { graphql, Link } from "gatsby"
+import rehypeReact from "rehype-react"
+import AppLayout from "../components/AppLayout"
+import Divider from "@material-ui/core/Divider"
+import { makeStyles } from "@material-ui/core"
+import Button from "@material-ui/core/Button"
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles(theme => ({
-    title: {
-        color: '#444444',
-        fontWeight: theme.typography.fontWeightMedium,
-    },
-    subTitle: {
-        color: '#5e5e5e',
-        fontWeight: theme.typography.fontWeightBold,
-    },
-
-}));
+  title: {
+    color: "#444444",
+    fontWeight: theme.typography.fontWeightMedium,
+  },
+  subTitle: {
+    color: "#5e5e5e",
+    fontWeight: theme.typography.fontWeightBold,
+  },
+}))
 
 const renderAst = new rehypeReact({
-    createElement: React.createElement,
-    components: {
-    }
-}).Compiler;
+  createElement: React.createElement,
+  components: {},
+}).Compiler
 
-export default function DocPage({data, location}) {
-    const classes = useStyles();
-    const post = data.markdownRemark;
+export default function DocPage({ data, location, pageContext }) {
+  const classes = useStyles()
+  const post = data.markdownRemark
+  const { next, prev } = pageContext
 
-    return (
-        <AppLayout title={post.frontmatter.title + ' | ' + data.site.siteMetadata.title} location={location}>
-            <Typography variant="h4" className={classes.title} gutterBottom>
-                {post.frontmatter.title}
-            </Typography>
-            <Typography variant="overline" display="block" className={classes.subTitle} gutterBottom>
-                {post.frontmatter.sub_title}
-            </Typography>
-            <Divider/>
-            <Typography variant="body1" gutterBottom>
-                {renderAst(post.htmlAst)}
-            </Typography>
-        </AppLayout>
-    )
+  return (
+    <AppLayout
+      title={post.frontmatter.title + " | " + data.site.siteMetadata.title}
+      location={location}
+    >
+      <Typography variant="h4" className={classes.title} gutterBottom>
+        {post.frontmatter.title}
+      </Typography>
+      <Typography
+        variant="overline"
+        display="block"
+        className={classes.subTitle}
+        gutterBottom
+      >
+        {post.frontmatter.sub_title}
+      </Typography>
+      <Divider />
+      <Typography variant="body1" gutterBottom>
+        {renderAst(post.htmlAst)}
+      </Typography>
+      {prev.fields && (
+        <Button variant="contained" color="primary" startIcon={<NavigateBeforeIcon />} href={prev.fields.slug} >
+          {prev.frontmatter.title}
+        </Button>
+      )}
+
+      {next.fields && (
+        <Button variant="contained" color="primary" startIcon={<NavigateNextIcon />} href={next.fields.slug}>
+          {next.frontmatter.title}
+        </Button>
+      )}
+    </AppLayout>
+  )
 }
 
 export const query = graphql`
@@ -52,7 +72,7 @@ export const query = graphql`
         title
       }
     }
-    markdownRemark(fields: { slug: { eq: $slug }}) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       htmlAst
       frontmatter {
         title
@@ -61,4 +81,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
