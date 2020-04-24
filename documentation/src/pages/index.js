b065fa448eac9b8f08480bc5492dbf99322a4d39
Kushang Gajjar <kgajjar@salesforce.com>
2020-04-24 17:38:12 -0400

    docs

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -1,173 +1,173 @@
-import React from "react"
-import AppLayout from "../components/AppLayout"
-import AppCard from "../components/AppCard"
-import codeceptjsBddFrameworkImage from "../images/codeceptbdd-arch.png"
-import howImage from "../images/How.png"
-import BDD from "../images/BDD.png"
-import autoRetry from "../images/autoRetry.png"
-import driversImage from "../images/driverAgnostic.png"
-import pluginBasedImage from "../images/pluginBased.png"
-import cucumber from "../images/cucumber1.png"
-import quickStartYt from "../images/quick-setup-yt.png"
-import runParallelYt from "../images/run-parallel-yt.png"
-import runMultiYt from "../images/run-multi-yt.png"
-import runSauceYt from "../images/run-sauce-yt.png"
-import YouTubeIcon from "@material-ui/icons/YouTube"
-import Hidden from "@material-ui/core/Hidden"
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import AppCard from '../components/AppCard';
+import codeceptjsBddFrameworkImage from '../images/codeceptbdd-arch.png';
+import howImage from '../images/How.png';
+import BDD from '../images/BDD.png';
+import autoRetry from '../images/autoRetry.png';
+import driversImage from '../images/driverAgnostic.png';
+import pluginBasedImage from '../images/pluginBased.png';
+import cucumber from '../images/cucumber1.png';
+import quickStartYt from '../images/quick-setup-yt.png';
+import runParallelYt from '../images/run-parallel-yt.png';
+import runMultiYt from '../images/run-multi-yt.png';
+import runSauceYt from '../images/run-sauce-yt.png';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import Hidden from '@material-ui/core/Hidden';
 
-import multibrowsers from "../images/multi-browsers.png"
-import saucelabs from "../images/saucelabs1.png"
-import { Divider } from "@material-ui/core"
-import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardHeader from "@material-ui/core/CardHeader"
-import CardMedia from "@material-ui/core/CardMedia"
-import CardContent from "@material-ui/core/CardContent"
-import CardActions from "@material-ui/core/CardActions"
-import Avatar from "@material-ui/core/Avatar"
-import Grid from "@material-ui/core/Grid"
-import Button from "@material-ui/core/Button"
-import DomainIcon from "@material-ui/icons/Domain"
-import VisibilityIcon from "@material-ui/icons/Visibility"
-import Paper from "@material-ui/core/Paper"
+import multibrowsers from '../images/multi-browsers.png';
+import saucelabs from '../images/saucelabs1.png';
+import { Divider } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import DomainIcon from '@material-ui/icons/Domain';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    display: "flex",
-    flexWrap: "wrap",
-    "& > *": {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
       margin: theme.spacing(1),
       width: theme.spacing(16),
       height: theme.spacing(16),
     },
   },
   card: {
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
   },
   media: {
     height: 0,
-    paddingTop: "56.25%", // 16:9
+    paddingTop: '56.25%', // 16:9
   },
   avatar: {
-    backgroundColor: "red",
+    backgroundColor: 'red',
   },
   arch: {
     paddingTop: 30,
   },
-}))
+}));
 
 const renderAppCards = () => {
   const appCardData = [
     {
       size: 4,
-      title: "Why BDD?",
+      title: 'Why BDD?',
       description:
-        "BDD is a great way to colloborate within teams & with stakeholders to create shared understanding of how the product should behave, capture concrete examples and create an executable & living specification on-the-fly.",
-      link: "https://medium.com/hackernoon/bdd-in-3-minutes-c3f8fc022237",
+        'BDD is a great way to colloborate within teams & with stakeholders to create shared understanding of how the product should behave, capture concrete examples and create an executable & living specification on-the-fly.',
+      link: 'https://medium.com/hackernoon/bdd-in-3-minutes-c3f8fc022237',
       image: BDD,
     },
     {
       size: 4,
-      title: "Write Feature Files",
+      title: 'Write Feature Files',
       description:
-        "CodeceptJs BDD integrates Cucumber to write Gherkin Feature Files. Cucumber is a popular collaboration tool used by many teams practiving BDD to capture requirements and create executable specifications.",
-      link: "/3-bdd-cucumber/1-feature/",
+        'CodeceptJs BDD integrates Cucumber to write Gherkin Feature Files. Cucumber is a popular collaboration tool used by many teams practiving BDD to capture requirements and create executable specifications.',
+      link: '/3-bdd-cucumber/1-feature/',
       image: cucumber,
     },
     {
       size: 4,
-      title: "Execute On Cloud",
+      title: 'Execute On Cloud',
       description:
-        "CodeceptJs BDD integrates Sauce Labs to execute tests on 900+ Desktop/Mobile browses, OS & Devices combinations. Tests can be executed locally or on Sauce Labs Browsers for broader coverage.",
-      link: "/execution/3-run-on-saucelabs/",
+        'CodeceptJs BDD integrates Sauce Labs to execute tests on 900+ Desktop/Mobile browses, OS & Devices combinations. Tests can be executed locally or on Sauce Labs Browsers for broader coverage.',
+      link: '/execution/3-run-on-saucelabs/',
       image: saucelabs,
     },
     {
       size: 3,
-      title: "Driver Agnostic",
+      title: 'Driver Agnostic',
       description:
-        "Run your tests via WebDriver and Playwright. The code is the same. It is easy to change the driver through configuration. Codecpetjs-BDD supports Pupetter, Headless Chrome, Selenium Webdriver and Webdriver IO.",
-      link: "https://codecept.io/plugins/#retryfailedstep",
+        'Run your tests via WebDriver and Playwright. The code is the same. It is easy to change the driver through configuration. Codecpetjs-BDD supports Pupetter, Headless Chrome, Selenium Webdriver and Webdriver IO.',
+      link: 'https://codecept.io/plugins/#retryfailedstep',
       image: driversImage,
     },
     {
       size: 3,
-      title: "Reduced Flakiness",
+      title: 'Reduced Flakiness',
       description:
-        "Codeceptjs has in-built Smart Wait and Auto Retry features for the elements that do not load in-time or elements that fails. Codeceptjs-BDD framework has configured these features that reduces the UI Test Flakiness at low level during DOM Element evaluation.",
-      link: "https://codecept.io/plugins/#retryfailedstep",
+        'Codeceptjs has in-built Smart Wait and Auto Retry features for the elements that do not load in-time or elements that fails. Codeceptjs-BDD framework has configured these features that reduces the UI Test Flakiness at low level during DOM Element evaluation.',
+      link: 'https://codecept.io/plugins/#retryfailedstep',
       image: autoRetry,
     },
     {
       size: 3,
-      title: "Plugin Based",
+      title: 'Plugin Based',
       description:
-        "Codeceptjs is a plugin based architecture. You can create helpers and plugins for your need and easily plug in to the framework. Codeceptjs-bdd implements two plugins: codeceptjs-share to share config and codeceptjs-saucelabs to integrate Sauce Labs.",
-      link: "https://github.com/gkushang/codeceptjs-bdd/tree/develop/packages",
+        'Codeceptjs is a plugin based architecture. You can create helpers and plugins for your need and easily plug in to the framework. Codeceptjs-bdd implements two plugins: codeceptjs-share to share config and codeceptjs-saucelabs to integrate Sauce Labs.',
+      link: 'https://github.com/gkushang/codeceptjs-bdd/tree/develop/packages',
       image: pluginBasedImage,
     },
     {
       size: 3,
-      title: "Multibrowsers | Parallel",
+      title: 'Multibrowsers | Parallel',
       description:
-        "Codeceptjs BDD provides an ability to run BDD Feaure files all in Parallel on Single browser or can run same set of tests in Parallel but on different browsers/OS combo (multile browers) on Sauce Labs. It offers greate Test Execution Coverage for your App.",
-      link: "/5-execution/5-run-on-multi-browsers/",
+        'Codeceptjs BDD provides an ability to run BDD Feaure files all in Parallel on Single browser or can run same set of tests in Parallel but on different browsers/OS combo (multile browers) on Sauce Labs. It offers greate Test Execution Coverage for your App.',
+      link: '/5-execution/5-run-on-multi-browsers/',
       image: multibrowsers,
     },
     {
       size: 3,
-      title: "Quick Setup Wizard",
+      title: 'Quick Setup Wizard',
       description:
-        "Quicky setup BDD framework for your App. Codecept-BDD implemetns the Interactive CLI to quickly setup the BDD Framework, integration with Sauce Labs and provides set of example Automated Feature files.",
+        'Quicky setup BDD framework for your App. Codecept-BDD implemetns the Interactive CLI to quickly setup the BDD Framework, integration with Sauce Labs and provides set of example Automated Feature files.',
       link:
-        "https://www.youtube.com/watch?time_continue=1&v=OGrn1ejyb-k&feature=emb_logo",
-      firstLinkName: "Watch in Action",
+        'https://www.youtube.com/watch?time_continue=1&v=OGrn1ejyb-k&feature=emb_logo',
+      firstLinkName: 'Watch in Action',
       icon: <YouTubeIcon color="secondary" />,
       image: quickStartYt,
     },
     {
       size: 3,
-      title: "Parallel Executions",
+      title: 'Parallel Executions',
       description:
-        "Run all your Feature files in Parallel. Codeceptjs-BDD automatically calculates number of threads to spun based on # of Features. You can choose to run them on either Sauce Labs or Locally installed browsers.",
-      link: "https://www.youtube.com/watch?v=he0_wn-xPGI&feature=emb_logo",
-      firstLinkName: "Watch in Action",
+        'Run all your Feature files in Parallel. Codeceptjs-BDD automatically calculates number of threads to spun based on # of Features. You can choose to run them on either Sauce Labs or Locally installed browsers.',
+      link: 'https://www.youtube.com/watch?v=he0_wn-xPGI&feature=emb_logo',
+      firstLinkName: 'Watch in Action',
       icon: <YouTubeIcon color="secondary" />,
       image: runParallelYt,
     },
     {
       size: 3,
-      title: "Multi Browsers",
+      title: 'Multi Browsers',
       description:
-        "Run your scenarios on Multiple Browsers in Parallel, including multiple versions of same brwoser or different browsers and different OS combinations on Sauce Labs. Boost your Test Execution coverage.",
-      link: "https://www.youtube.com/watch?v=njOlOJ07Dxw",
-      firstLinkName: "Watch in Action",
+        'Run your scenarios on Multiple Browsers in Parallel, including multiple versions of same brwoser or different browsers and different OS combinations on Sauce Labs. Boost your Test Execution coverage.',
+      link: 'https://www.youtube.com/watch?v=njOlOJ07Dxw',
+      firstLinkName: 'Watch in Action',
       icon: <YouTubeIcon color="secondary" />,
       image: runMultiYt,
     },
     {
       size: 3,
-      title: "Run on Sauce Labs",
+      title: 'Run on Sauce Labs',
       description:
-        "Codeceptjs-BDD framework integrates the cloud based platform Sauce Labs to execute scenarios on 900+ Desktop/Mobile browses, OS & Devices combinations, providing Greater Scenarios Execution Coverage.",
-      link: "https://www.youtube.com/watch?v=ugCjMOJlClc",
-      firstLinkName: "Watch in Action",
+        'Codeceptjs-BDD framework integrates the cloud based platform Sauce Labs to execute scenarios on 900+ Desktop/Mobile browses, OS & Devices combinations, providing Greater Scenarios Execution Coverage.',
+      link: 'https://www.youtube.com/watch?v=ugCjMOJlClc',
+      firstLinkName: 'Watch in Action',
       icon: <YouTubeIcon color="secondary" />,
       image: runSauceYt,
     },
-  ]
+  ];
 
   return appCardData.map(cardData => (
     <Grid item sm={cardData.size}>
       <AppCard {...cardData} />
     </Grid>
-  ))
-}
+  ));
+};
 
 const renderCardDataWithActions = classes => {
   const cardDataWithActions = [
@@ -175,36 +175,36 @@ const renderCardDataWithActions = classes => {
       cardHeader: {
         avatar: (
           <Avatar aria-label="architecture" className={classes.avatar}>
-            {" "}
-            <DomainIcon />{" "}
+            {' '}
+            <DomainIcon />{' '}
           </Avatar>
         ),
-        title: "Framework Architecture",
-        subheader: "Codeceptjs BDD",
+        title: 'Framework Architecture',
+        subheader: 'Codeceptjs BDD',
       },
       cardMedia: {
         className: classes.media,
         image: codeceptjsBddFrameworkImage,
-        title: "Architecture",
+        title: 'Architecture',
       },
     },
     {
       cardHeader: {
         avatar: (
           <Avatar aria-label="how to usage" className={classes.avatar}>
-            <VisibilityIcon />{" "}
+            <VisibilityIcon />{' '}
           </Avatar>
         ),
-        title: "Flow",
-        subheader: "Framework",
+        title: 'Flow',
+        subheader: 'Framework',
       },
       cardMedia: {
         className: classes.media,
         image: howImage,
-        title: "How To",
+        title: 'How To',
       },
     },
-  ]
+  ];
 
   return cardDataWithActions.map(cardData => (
     <Grid item xs={12} className={classes.arch}>
@@ -240,11 +240,11 @@ const renderCardDataWithActions = classes => {
         </CardActions>
       </Card>
     </Grid>
-  ))
-}
+  ));
+};
 
 const IndexPage = () => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <AppLayout title="Codeceptjs-BDD">
@@ -268,24 +268,25 @@ const IndexPage = () => {
         >
           <div
             style={{
-              color: "#2F4B4B",
-              fontSize: "1em",
-
-              marginTop: "1px",
-              fontStyle: "regular",
+              color: '#2F4B4B',
+              fontSize: '0.85em',
+              fontFamily:
+                'helveticaneue-light, helvetica neue light, helvetica neue, Helvetica, Arial, lucida grande, sans-serif',
+              marginTop: '1px',
             }}
           >
             Quickly create your Codeceptjs-BDD Acceptance tests through open
             source
             <span
               style={{
-                color: "#253C3C",
-                fontFamily: "monospace, monospace, monospace",
-                fontStyle: "bold",
+                color: '#253C3C',
+                fontSize: '1em',
+                fontFamily: 'monospace, monospace, monospace',
+                fontWeight: '500',
               }}
             >
-              {" "}
-              create-codecepjts-bdd-tests{" "}
+              {' '}
+              create-codecepjts-bdd-tests{' '}
             </span>
             tool.
           </div>
@@ -296,28 +297,28 @@ const IndexPage = () => {
             <Paper
               elevation={8}
               style={{
-                backgroundColor: "#2F4B4B",
-                height: "5em",
-                fontColor: "yelllow",
+                backgroundColor: '#2F4B4B',
+                height: '5em',
+                fontColor: 'yelllow',
               }}
             >
               <Hidden smUp>
                 <div
                   style={{
-                    color: "#e1e31a",
-                    fontSize: "14px",
-                    marginTop: "5px",
-                    fontFamily: "monospace, monospace, monospace",
-                    paddingTop: "1.7em",
-                    paddingLeft: "10px",
-                    overflowX: "scroll",
-                    whiteSpace: "nowrap",
+                    color: '#e1e31a',
+                    fontSize: '14px',
+                    marginTop: '5px',
+                    fontFamily: 'monospace, monospace, monospace',
+                    paddingTop: '1.7em',
+                    paddingLeft: '10px',
+                    overflowX: 'scroll',
+                    whiteSpace: 'nowrap',
                   }}
                 >
-                  <span style={{ color: "#e1e31a" }}>$ </span>
-                  <span style={{ color: "white" }}>npx </span>
-                  <span style={{ color: "#ffe59a" }}>
-                    create-codeceptjs-bdd-tests{" "}
+                  <span style={{ color: '#e1e31a' }}>$ </span>
+                  <span style={{ color: 'white' }}>npx </span>
+                  <span style={{ color: '#ffe59a' }}>
+                    create-codeceptjs-bdd-tests{' '}
                   </span>
                 </div>
               </Hidden>
@@ -325,18 +326,18 @@ const IndexPage = () => {
               <Hidden xsDown>
                 <div
                   style={{
-                    color: "#e1e31a",
-                    fontSize: "1.7em",
-                    marginTop: "5px",
-                    fontFamily: "monospace, monospace, monospace",
-                    paddingTop: "0.8em",
-                    paddingLeft: "1em",
+                    color: '#e1e31a',
+                    fontSize: '1.7em',
+                    marginTop: '5px',
+                    fontFamily: 'monospace, monospace, monospace',
+                    paddingTop: '0.8em',
+                    paddingLeft: '1em',
                   }}
                 >
-                  <span style={{ color: "#e1e31a" }}>$ </span>
-                  <span style={{ color: "white" }}>npx </span>
-                  <span style={{ color: "#ffe59a" }}>
-                    create-codeceptjs-bdd-tests{" "}
+                  <span style={{ color: '#e1e31a' }}>$ </span>
+                  <span style={{ color: 'white' }}>npx </span>
+                  <span style={{ color: '#ffe59a' }}>
+                    create-codeceptjs-bdd-tests{' '}
                   </span>
                 </div>
               </Hidden>
@@ -350,7 +351,7 @@ const IndexPage = () => {
         {renderCardDataWithActions(classes)}
       </div>
     </AppLayout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
