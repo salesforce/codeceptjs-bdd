f683e527141782497c293bb75f44256fe9b5970e
Kushang Gajjar <g.kushang@gmail.com>
2020-09-14 19:11:18 -0400

    optimize steps and pages

--- packages/codeceptjs-shared/lib/config/bdd/pageObjects.js
+++ packages/codeceptjs-shared/lib/config/bdd/pageObjects.js
@@ -4,9 +4,11 @@ const camelCase = require('camelcase');
 
 const pageObjects = () => {
     let pages = {};
+
     glob.sync(path.join(process.cwd(), process.env.CODECEPT_RELATIVE_PATH, '/pages/**/*.page.js')).map((file) => {
         pages[camelCase(path.parse(file).name)] = file;
     });
+
     return pages;
 };
 
