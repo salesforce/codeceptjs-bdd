e87d3ec8ebf9344a2ebf6cdc47e2f74e366983f4
Kushang Gajjar <g.kushang@gmail.com>
2020-10-06 22:26:57 -0400

    circle ci

--- packages/create-codeceptjs-bdd-tests/acceptance/mocha/login.mocha.spec.js
+++ packages/create-codeceptjs-bdd-tests/acceptance/mocha/login.mocha.spec.js
@@ -1,7 +1,9 @@
 Feature('Login Tests (mocha)');
 
 Scenario('Fred logs in successfully', (I) => {
+
         I.amOnPage('/gkushang/codeceptjs-bdd');
+        
     })
     .tag('@login_mocha')
     .tag('smoke');
