3699325b12bb486c5de1b635adcbea58220b4e3b
Kushang Gajjar <g.kushang@gmail.com>
2020-10-08 01:13:39 -0400

    fix the driver env

--- packages/create-codeceptjs-bdd-tests/cli/update.env.js
+++ packages/create-codeceptjs-bdd-tests/cli/update.env.js
@@ -4,69 +4,30 @@ const path = require('path');
 
 class UpdateEnvironments {
     constructor(envs) {
-        const sourceSecrets = path.join(
-            envs.rootPath,
-            envs.relativePath,
-            `acceptance`,
-            'config',
-            'codecept.dev.env.example'
-        );
+        const sourceSecrets = path.join(envs.rootPath, envs.relativePath, `acceptance`, 'config', 'codecept.dev.env.example');
 
-        const destinationSecrets = path.join(
-            envs.rootPath,
-            envs.relativePath,
-            `acceptance`,
-            'config',
-            'codecept.dev.env'
-        );
+        const destinationSecrets = path.join(envs.rootPath, envs.relativePath, `acceptance`, 'config', 'codecept.dev.env');
 
         fs.copyFileSync(sourceSecrets, destinationSecrets);
 
-        this.codeceptDefaults = path.join(
-            envs.rootPath,
-            envs.relativePath,
-            `acceptance`,
-            'config',
-            'codecept.env'
-        );
+        this.codeceptDefaults = path.join(envs.rootPath, envs.relativePath, `acceptance`, 'config', 'codecept.env');
 
-        this.codeceptSecrets = path.join(
-            envs.rootPath,
-            envs.relativePath,
-            `acceptance`,
-            'config',
-            'codecept.dev.env'
-        );
+        this.codeceptSecrets = path.join(envs.rootPath, envs.relativePath, `acceptance`, 'config', 'codecept.dev.env');
     }
 
     driver = (envs) => {
-        shell.sed('-i', 'webdriver', envs.driver, this.codeceptDefaults);
+        shell.sed('-i', 'playwright', envs.driver, this.codeceptDefaults);
     };
 
     relativePath = (envs) => {
         // update relative path
-        shell.sed(
-            '-i',
-            './acceptance/',
-            './' + envs.relativePath + '/acceptance/',
-            this.codeceptDefaults
-        );
+        shell.sed('-i', './acceptance/', './' + envs.relativePath + '/acceptance/', this.codeceptDefaults);
     };
 
     sauceLabs = (envs) => {
-        shell.sed(
-            '-i',
-            '<enter-your-saucelabs-username>',
-            envs.sauceUsername,
-            this.codeceptSecrets
-        );
+        shell.sed('-i', '<enter-your-saucelabs-username>', envs.sauceUsername, this.codeceptSecrets);
 
-        shell.sed(
-            '-i',
-            '<enter-your-saucelabs-accesskey>',
-            envs.sauceKey,
-            this.codeceptSecrets
-        );
+        shell.sed('-i', '<enter-your-saucelabs-accesskey>', envs.sauceKey, this.codeceptSecrets);
     };
 
     updateHost = (envs) => {};
