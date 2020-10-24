341c3fb480eec81f55ad454d779bef9c333920fa
Kushang <kgajjar@salesforce.com>
2020-10-24 01:33:26 -0400

    conf.dev.env should be ignored

--- packages/codeceptjs-configure/lib/host/host.js
+++ packages/codeceptjs-configure/lib/host/host.js
@@ -5,16 +5,12 @@
 const fs = require('fs');
 const path = require('path');
 
-if (!fs.existsSync(path.join(process.env.CODECEPT_RELATIVE_PATH, 'config', 'codecept.dev.env'))) {
-    throw new Error("FILE NOT EXISTS");
-}
-
 const get = function (defaultHost, scheme) {
     let host = process.env.HOST ? process.env.HOST : defaultHost;
     scheme = scheme ? scheme : 'https';
 
     if (!host) {
-        throw Error('HOST is not defined in ENV variable. Check if "./acceptance/config/codecept.dev.env" file exists. If not, please create the file and define your enviroment varialbles. More info https://gkushang.github.io/04-configurations/1-env-variables/');
+        throw Error('HOST is not defined in ENV variable. Check if "HOST" is defined in either "./acceptance/config/codecept.env" or "./acceptance/config/codecept.dev.env" file. More info https://gkushang.github.io/04-configurations/1-env-variables/');
     }
 
     if (!host.match(/^[a-zA-Z]+:\/\//)) {
