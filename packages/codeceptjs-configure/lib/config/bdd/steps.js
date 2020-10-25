4aaec1ed6168f976a5faeb3d9bcf7c81779c9284
Kushang <kgajjar@salesforce.com>
2020-10-25 16:26:12 -0400

    add hoooks

--- packages/codeceptjs-configure/lib/config/bdd/steps.js
+++ packages/codeceptjs-configure/lib/config/bdd/steps.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const steps = () => {
     let steps = [];
-    glob.sync(path.join(process.cwd(), process.env.CODECEPT_RELATIVE_PATH, 'steps/**/*.steps.js')).map((file) => steps.push(file));
+    glob.sync(path.join(process.cwd(), process.env.CODECEPT_RELATIVE_PATH, '{steps/**/*.steps.js,__specs__/hooks/**/*.hooks.js}')).map((file) => steps.push(file));
+    // glob.sync(path.join(process.cwd(), process.env.CODECEPT_RELATIVE_PATH, '__specs__/hooks/**/*.hooks.js')).map((file) => steps.push(file));
     return steps;
 };
 
