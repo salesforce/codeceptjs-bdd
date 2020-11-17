31172086714d42197403d13247f426cc99245e77
Kushang Gajjar <g.kushang@gmail.com>
2020-11-16 19:52:01 -0500

    precommit hooks

--- packages/codeceptjs-configure/lib/host/host.js
+++ packages/codeceptjs-configure/lib/host/host.js
@@ -2,8 +2,8 @@
  * get host
  *
  */
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 const get = function (defaultHost, scheme) {
     let host = process.env.HOST ? process.env.HOST : defaultHost;
