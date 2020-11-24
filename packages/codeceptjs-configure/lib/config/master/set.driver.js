847d496acd407e74ac58fc6e97d4506e3ee3b5ad
Kushang Gajjar <g.kushang@gmail.com>
2020-11-24 16:01:59 -0500

    add automatic selection of driver

--- packages/codeceptjs-configure/lib/config/master/set.driver.js
+++ packages/codeceptjs-configure/lib/config/master/set.driver.js
@@ -1,5 +1,11 @@
 module.exports = function () {
     if (process.env.profile && process.env.profile.match('sauce:appium:[a-zA-Z]')) {
         process.env.DRIVER = 'Appium';
+    } else if (process.env.profile && process.env.profile.match('sauce:[a-zA-Z]')) {
+        process.env.DRIVER = 'Webdriver';
+    } else if (process.env.profile && process.env.profile.match('selenoid:[a-zA-Z]')) {
+        process.env.DRIVER = 'Webdriver';
+    } else if (process.env.profile && process.env.profile.match('device:[a-zA-Z]')) {
+        process.env.DRIVER = 'Playwright';
     }
 };
