e7ac6c930a4b27361f060067d2e287d9f92e16c7
Kushang Gajjar <g.kushang@gmail.com>
2020-11-30 15:57:39 -0500

    run on webdriver and playwright thru profile

--- packages/codeceptjs-configure/lib/config/master/set.driver.js
+++ packages/codeceptjs-configure/lib/config/master/set.driver.js
@@ -7,5 +7,9 @@ module.exports = function () {
         process.env.DRIVER = 'Webdriver';
     } else if (process.env.profile && process.env.profile.match('device:[a-zA-Z]')) {
         process.env.DRIVER = 'Playwright';
+    } else if (process.env.profile && process.env.profile.match('webdriver:[a-zA-Z]')) {
+        process.env.DRIVER = 'Webdriver';
+    } else if (process.env.profile && process.env.profile.match('playwright:[a-zA-Z]')) {
+        process.env.DRIVER = 'Playwright';
     }
 };
