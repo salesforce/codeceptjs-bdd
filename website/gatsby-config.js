f13425e020b44d30104d48aa0a76a4e635440529
Kushang Gajjar <g.kushang@gmail.com>
2020-12-06 12:18:07 -0500

    website fix

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-material-ui`,
-    {
-      resolve: 'gatsby-plugin-styled-components',
-      options: {
-        fileName: false,
-        pure: true,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -28,7 +21,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'docs',
-        path: `${__dirname}/docs`,
+        path: `${__dirname}/docs/`,
       },
     },
     {
@@ -41,60 +34,19 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        classPrefix: 'language-',
-        inlineCodeMarker: null,
-        aliases: {},
-        showLineNumbers: false,
-        noInlineHighlight: false,
-        languageExtensions: [
-          {
-            language: 'superscript',
-            extend: 'javascript',
-            definition: {
-              superscript_types: /(SuperType)/,
-            },
-            insertBefore: {
-              function: {
-                superscript_keywords: /(superif|superelse)/,
-              },
-            },
-          },
-        ],
-        prompt: {
-          user: 'root',
-          host: 'localhost',
-          global: false,
-        },
-        escapeEntities: {},
         plugins: [
           {
-            resolve: `gatsby-plugin-styled-components`,
+            resolve: 'gatsby-remark-images',
             options: {
-              // Add any options here
+              maxWidth: 690,
             },
           },
           {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-              plugins: [
-                {
-                  resolve: `gatsby-remark-images`,
-                  options: {
-                    maxWidth: 756,
-                  },
-                },
-                {
-                  resolve: `gatsby-remark-responsive-iframe`,
-                  options: {
-                    wrapperStyle: `margin-bottom: 1.0725rem`,
-                  },
-                },
-                `gatsby-remark-copy-linked-files`,
-                `gatsby-remark-smartypants`,
-                `gatsby-remark-prismjs`,
-              ],
-            },
+            resolve: 'gatsby-remark-responsive-iframe',
           },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-autolink-headers',
         ],
       },
     },
@@ -113,6 +65,7 @@ module.exports = {
         icon: `src/images/favicon.png`,
       },
     },
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-offline`,
     {
       resolve: '@gatsby-contrib/gatsby-plugin-elasticlunr-search',
