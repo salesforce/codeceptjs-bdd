8044d5ae73e813fa1370c1f27507293a048ce070
Kushang Gajjar <g.kushang@gmail.com>
2021-01-03 14:13:49 -0500

    disable allure helper

--- packages/create-codeceptjs-bdd-tests/steps.d.ts
+++ packages/create-codeceptjs-bdd-tests/steps.d.ts
@@ -4,11 +4,10 @@ type ghSearchPage = typeof import('./acceptance/pages/github/gh-search.page');
 type page = typeof import('codeceptjs-configure/lib/helpers/global.page.js');
 type Driver_commands = import('/Users/kgajjar/dev/b2c/codeceptjs-bdd/packages/codeceptjs-configure/lib/helpers/driver-commands.helper.js');
 type custom_methods = import('/Users/kgajjar/dev/b2c/codeceptjs-bdd/packages/codeceptjs-configure/lib/helpers/custom-methods.helper.js');
-type allure_report_helper = import('/Users/kgajjar/dev/b2c/codeceptjs-bdd/packages/codeceptjs-configure/lib/helpers/allure-report.helper.js');
 
 declare namespace CodeceptJS {
   interface SupportObject { I: I, current: any, ghHomePage: ghHomePage, ghSearchPage: ghSearchPage, page: page }
-  interface Methods extends Driver_commands, custom_methods, allure_report_helper, REST, Playwright {}
+  interface Methods extends Driver_commands, custom_methods, REST, WebDriver {}
   interface I extends WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
