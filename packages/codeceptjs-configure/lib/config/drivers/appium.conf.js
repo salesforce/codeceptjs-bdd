bf8365af53a84faf31bdf9dc3b82c47c3c595922
Kushang Gajjar <g.kushang@gmail.com>
2021-02-09 13:18:51 -0500

    feat(appium): appium support for custom locators

--- packages/codeceptjs-configure/lib/config/drivers/appium.conf.js
+++ packages/codeceptjs-configure/lib/config/drivers/appium.conf.js
@@ -1,10 +1,17 @@
 const merge = require('deepmerge');
 const host = require('../../host/host');
+const { locatorStrategy } = require('query-selector-shadow-dom/plugins/webdriverio');
 
 const appium_conf = {
     helpers: {
         Appium: {
             url: host.get(),
+            smartWait: 5000,
+            waitForTimeout: 25000,
+            customLocatorStrategy:
+                process.env.CUSTOM_LOCATOR_STRATEGY === true || process.env.CUSTOM_LOCATOR_STRATEGY === 'true'
+                    ? locatorStrategy
+                    : undefined,
         },
     },
 };
