0ab94d314d3d3fb44c14c5c912bfb50db4c04b01
Kushang <kgajjar@salesforce.com>
2021-03-21 14:32:11 -0400

    feat(allure): allure launcher for windows

--- packages/codeceptjs-configure/lib/reports/allure-static-launcher/win/launch_html_report.bat
+++ packages/codeceptjs-configure/lib/reports/allure-static-launcher/win/launch_html_report.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-SET PARENTDIR=abcd %cd%   
+SET PARENTDIR=%cd%   
 for /f "tokens=* delims= " %%a in ("%PARENTDIR%") do set PARENTDIR=%%a
 for /l %%a in (1,1,100) do if "!PARENTDIR:~-1!"==" " set PARENTDIR=!PARENTDIR:~0,-1!
 echo. "%PARENTDIR%\report\bin" 
