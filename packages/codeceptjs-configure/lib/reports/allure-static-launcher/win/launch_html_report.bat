52c86abed62ed2610f7a38c37953a3268a686ade
Kushang Gajjar <g.kushang@gmail.com>
2021-03-21 23:57:06 -0400

    feat(allure): report launcher

--- packages/codeceptjs-configure/lib/reports/allure-static-launcher/win/launch_html_report.bat
+++ packages/codeceptjs-configure/lib/reports/allure-static-launcher/win/launch_html_report.bat
@@ -5,5 +5,5 @@ SET PARENTDIR=%cd%
 for /f "tokens=* delims= " %%a in ("%PARENTDIR%") do set PARENTDIR=%%a
 for /l %%a in (1,1,100) do if "!PARENTDIR:~-1!"==" " set PARENTDIR=!PARENTDIR:~0,-1!
 echo. "%PARENTDIR%\report\bin" 
-cd %PARENTDIR%\report\bin 
-allure serve %PARENTDIR%\report
+cd "%PARENTDIR%\report\bin" 
+allure serve "%PARENTDIR%\report"
