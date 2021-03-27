f5dc411c2a5b7fadbf1eb908763d6fd85f62eb6a
Kushang <kgajjar@salesforce.com>
2021-03-27 18:22:43 -0400

    allure report launcher for windows

--- packages/codeceptjs-configure/lib/reports/allure-static-launcher/win/launch_html_report.bat
+++ packages/codeceptjs-configure/lib/reports/allure-static-launcher/win/launch_html_report.bat
@@ -5,5 +5,12 @@ SET PARENTDIR=%cd%
 for /f "tokens=* delims= " %%a in ("%PARENTDIR%") do set PARENTDIR=%%a
 for /l %%a in (1,1,100) do if "!PARENTDIR:~-1!"==" " set PARENTDIR=!PARENTDIR:~0,-1!
 echo. "%PARENTDIR%\report\bin" 
+
+where java >nul 2>nul
+if %errorlevel%==1 (
+    echo. "Error Java Not Found. Please install JDK from https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html"
+    exit
+)
+
 cd "%PARENTDIR%\report\bin" 
 allure serve "%PARENTDIR%\report"
