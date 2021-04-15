8cbabecae7a8d557f0fe0823e33753710636f26c
Kushang <kgajjar@salesforce.com>
2021-04-14 23:35:06 -0400

    disable the pdf viewer plugin

--- packages/codeceptjs-configure/lib/config/drivers/webdriver.conf.js
+++ packages/codeceptjs-configure/lib/config/drivers/webdriver.conf.js
@@ -68,6 +68,9 @@ const get = (conf) => {
         conf.helpers.WebDriver.desiredCapabilities = {
             chromeOptions: {
                 args: ['--disable-notifications'],
+                prefs: {
+                    'plugins.always_open_pdf_externally': true,
+                },
             },
         };
     }
