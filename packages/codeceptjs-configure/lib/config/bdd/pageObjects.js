3ca9d355532d136732e38ba82684ace19e3f3203
Kushang Gajjar <g.kushang@gmail.com>
2021-06-02 19:33:46 -0400

    feat: define page objects path

--- packages/codeceptjs-configure/lib/config/bdd/pageObjects.js
+++ packages/codeceptjs-configure/lib/config/bdd/pageObjects.js
@@ -4,11 +4,18 @@ const camelCase = require('camelcase');
 
 const pageObjects = (pathToPages) => {
     let pages = {};
+    let pageObjects = '';
+
     if (!pathToPages) {
         pathToPages = '/pages/**/*.page.{js,ts}';
+        pageObjects = path.join(process.env.CODECEPT_RELATIVE_PATH, pathToPages);
     }
 
-    glob.sync(path.join(process.env.CODECEPT_RELATIVE_PATH, pathToPages)).map((file) => {
+    if (pathToPages.from) {
+        pageObjects = pathToPages.from;
+    }
+
+    glob.sync(pageObjects).map((file) => {
         pages[camelCase(path.parse(file).name)] = './' + file;
     });
 
